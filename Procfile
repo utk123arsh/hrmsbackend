03b123db6c81c9efa5f6a5b402d5ff1a0bdbc397
Utkarsh Chaudhary <utkarshjaat01@gmail.com>
2026-02-26 17:03:17 +0530

    Add whitenoise for static file serving

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python manage.py migrate && python manage.py collectstatic --no-input
 web: gunicorn hrms.wsgi
--- hrms/settings.py
+++ hrms/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,8 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # CORS Configuration
 CORS_ALLOW_ALL_ORIGINS = True
