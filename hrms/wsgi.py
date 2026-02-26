f94a1ac806a90ad7eb269cf79dda78f0f8446336
Utkarsh Chaudhary <utkarshjaat01@gmail.com>
2026-02-26 17:19:26 +0530

    Auto-run migrations on startup

--- hrms/wsgi.py
+++ hrms/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hrms.settings')
 
 application = get_wsgi_application()
+
+# Run migrations on startup
+try:
+    call_command('migrate', verbosity=0)
+except Exception as e:
+    print(f"Migration error: {e}", file=sys.stderr)
